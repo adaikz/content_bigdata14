882ee7015dec9425deb4f581e562f0963093e3df
Andrey Chubin <72697554+andreychubin95@users.noreply.github.com>
2021-04-23 15:55:54 +0300

    Add files via upload

--- code-reviews/lab02/reviews/lab02_alexey.lubenets.py
+++ code-reviews/lab02/reviews/lab02_alexey.lubenets.py
@@ -1,54 +1,85 @@
-#!/usr/bin/env python
-# извелечение требуемого из входного набора
+# base_creator.py
+
+#!/opt/anaconda/envs/bd9/bin/python3
+import happybase
+
+
+def main():
+    connection = happybase.Connection('bd-master.newprolab.com')
+    try:
+        connection.delete_table("sergey.korolev", disable=True)
+    except:
+        pass
+
+    urls = {
+        'UID': dict(),
+        'URL': dict()
+    }
+
+    connection.create_table("sergey.korolev", urls)
+
+
+if __name__ == "__main__":
+    main()
+
+# mapper.py
+
+#!/opt/anaconda/envs/bd9/bin/python3
 import sys
 
-for line in sys.stdin:
-    elements = line.split("\t")
-    try:
-        if len(elements) == 3 and elements[0].strip() not in '-' and (elements[2].strip()).startswith('http'):
-            (UID, timestamp, URL) = elements
-            if int(UID) % 256 == 10:
-                print(line)
-    except Exception:
-        continue
 
-#!/opt/anaconda/envs/bd9/bin/python
-# coding=utf8
+def main():
+    for line in sys.stdin:
+        line = line.split('\t')
+        if len(line) == 3:
+            UID, timestamp, URL = line
+            if (UID != '-') & URL.startswith('http') & (timestamp != '-') & (timestamp != '') & (UID != ''):
+                if int(UID) % 256 == 93:
+                    print(UID + " \t" + URL.strip() + "\t" + timestamp)
 
-# сохранение в hbase-базу
+
+if __name__ == "__main__":
+    main()
+
+# reducer.py
+
+#!/opt/anaconda/envs/bd9/bin/python3
 import happybase
 import sys
 
-table_name = 'alexey.lubenets'
 
-c = happybase.Connection(host='bd-master.newprolab.com')
-c.open()
+def main():
+    connection = happybase.Connection('bd-master.newprolab.com')
+    table = connection.table("sergey.korolev")
+    for line in sys.stdin:
+        line = line.split('\t')
+        if len(line) == 3:
+            UID, URL, timestamp = line
+            timestamp = int(float(timestamp) * 1000)
+            table.put(UID, data={"data:url": URL.strip()}, timestamp=timestamp)
 
-print(c.tables())
-try:
-    c.disable_table(table_name)
-    c.delete_table(table_name)
-except Exception:
-    print(Exception)
 
-c.create_table(table_name, {'data': dict(max_versions=4096)})
-table = c.table(table_name)
-print(table)
+if __name__ == "__main__":
+    main()
 
-for line in sys.stdin:
-    print(line)
-    items = line.split("\t")
-    if len(items) == 3:
-        (UID, ts, URL) = items
-        timestamp = int(float(ts) * 1000)
-        # print(timestamp)
-        table.put(UID, {'data:url': URL.strip()}, timestamp=timestamp)
-        print(line + ' is saved')
+# run.sh
 
-# проверка сохраненного
-for uid in ['3331220746', '3281840906']:
-    pairs = table.cells(uid, 'data:url', versions=30, timestamp=None, include_timestamp=True)
-    print(pairs)
+#!/usr/bin/env bash
 
-for key, data in table.scan():
-    print(str(key) + ': ' + str(data))
+# alexey.lubenets: 2021_04_22: думаю, что для решения данной лабы достаточно использования только маппера.
+# замечаний к оформлению нет, код понятный и структурированный => комментарии и не требуются
+
+'''
+OUT_DIR="/user/sergey.korolev/lab02s"
+NUM_REDUCERS=8
+
+python3 base_creator.py
+
+hadoop jar hadoop-streaming.jar \
+    -D mapreduce.job.reduces=$NUM_REDUCERS \
+    -files mapper.py,reducer_combiner.py,reducer.py \
+    -mapper "python3 mapper.py" \
+    -reducer "python3 reducer.py" \
+    -input /labs/lab02data/facetz_2015_02_05 \
+    -output ${OUT_DIR}.tmp > /dev/null
+'''
